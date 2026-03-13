8d65c3a1ea53f1894d927200fa569766b9144fd4
Yukkurisiteikitai <YukkuriorSience@gmail.com>
2026-03-13 13:41:58 +0900

    feat:x

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import Providers from './providers';
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://www.yourselflm.org'),
   title: 'YourselfLM',
   description: 'YourselfLMは、LLMを活用して自己理解を深めるためのツールです。ユーザーは、LLMとの対話を通じて自己分析や目標設定を行い、自己成長を促進します。',
   manifest: '/manifest.json',
@@ -17,11 +18,17 @@ export const metadata: Metadata = {
     url: 'https://www.yourselflm.org/',
     images: [
       {
-        url: 'https://www.yourselflm.org/favicon.ico',
-        width: 48,
-        height: 48,
+        url: '/YourselfLM_OGP.jpg',
+        width: 1200,
+        height: 630,
       },
     ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'YourselfLM',
+    description: 'YourselfLMは、LLMを活用して自己理解を深めるためのツールです。ユーザーは、LLMとの対話を通じて自己分析や目標設定を行い、自己成長を促進します。',
+    images: ['/YourselfLM_OGP.jpg'],
   },
 };
 
